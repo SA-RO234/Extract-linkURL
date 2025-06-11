d2bf2c9ff69efd38d0dd9df8b23c15b1c2980864
Sun Rosa <rozasun61@gmail.com>
2025-06-11 11:07:44 +0700

    Update

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,16 @@
 <?php
 
 error_reporting(E_ALL);
-ini_set('display_errors',1);
+ini_set('display_errors', 1);
 
-define("STORAGE_PATH",__DIR__."/../storage/");
+// Use /var/data/ if it exists (Render persistent disk), otherwise use local storage
+if (is_dir('/var/data/')) {
+    define("STORAGE_PATH", '/var/data/');
+} else {
+    define("STORAGE_PATH", __DIR__ . "/../storage/");
+}
 
 //  Define Specific file storage path 
-define('EMAIL_DATA_FILE',STORAGE_PATH.'emaildata.txt');
-define("PHONE_DATA_FILE",STORAGE_PATH.'phonedata.txt');
-define("IMAGE_DATA_FILE",STORAGE_PATH.'imagedata.txt');
-
-
+define('EMAIL_DATA_FILE', STORAGE_PATH . 'emaildata.txt');
+define("PHONE_DATA_FILE", STORAGE_PATH . 'phonedata.txt');
+define("IMAGE_DATA_FILE", STORAGE_PATH . 'imagedata.txt');
